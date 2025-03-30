c85e79ec371aec8c61b96f1b14e8cf084b92e759
xht308 <77667228+xht308@users.noreply.github.com>
2025-03-30 14:19:59 -0500

    Update build script and manifest for version 0.0.2 release

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-zip -r zotero-pdf-copy.xpi * -x "*.git/*" -x "*.xpi" -x "update.json" -x "*.tmpl"
+zip -r zotero-pdf-copy.xpi * -x "*.git/*" -x "*.xpi" -x "update.json" -x "*.tmpl" -x "build.sh"
 
-jq ".addons[\"zotero-pdf-copy@infty.fun\"].updates[0].update_hash = \"sha256:`shasum -a 256 zotero-pdf-copy.xpi | cut -d' ' -f1`\"" update.json.tmpl > update.json
+verison=$(jq -r '.version' manifest.json)
+
+jq \
+  --arg version "`jq -r '.version' manifest.json`" \
+  --arg hash "sha256:`shasum -a 256 zotero-pdf-copy.xpi | cut -d' ' -f1`" \
+  --arg update_link "https://github.com/xht308/zotero-pdf-copy/releases/download/v`jq -r '.version' manifest.json`/zotero-pdf-copy.xpi" \
+  '.addons["zotero-pdf-copy@infty.fun"].updates[0].version = $version |
+   .addons["zotero-pdf-copy@infty.fun"].updates[0].update_link = $update_link |
+   .addons["zotero-pdf-copy@infty.fun"].updates[0].update_hash = $hash' \
+  update.json.tmpl > update.json
